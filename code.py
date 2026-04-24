9a800010b17ead6e11023bbc5100c486bc898fbf
JayThakkar-7492 <cajaythakkar.backup@gmail.com>
2026-04-24 10:15:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,15 +2,14 @@ import qrcode
 import streamlit as st
 from PIL import Image
 
-# Title for your web app
-st.title("QR Code Generator")
-
-# Variable assigning
+# 1. Generate the QR code
 url = "https://www.pib.gov.in/indexd.aspx?reg=3&lang=2"
+qr_img = qrcode.make(url)
 
-# Process of making QR Code
-img = qrcode.make(url)
+# 2. Convert the 'qrcode' object into a standard PIL image
+# This ensures Streamlit can read the pixel data correctly
+standard_img = qr_img.get_image()
 
-# Output: Displaying on Streamlit
-# We use st.image to render the PIL object directly
-st.image(img, caption="Scan this to visit PIB News", use_container_width=True)
+# 3. Display it
+st.title("PIB News QR Code")
+st.image(standard_img, caption="Scan this to visit PIB News")
